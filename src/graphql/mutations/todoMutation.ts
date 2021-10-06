4c0a633cdc00293476227fef46b422572192468c
kritsada <newtronetic@gmail.com>
2021-10-06 17:08:49 +0700

    fix: change position todo card

--- src/graphql/mutations/todoMutation.ts
+++ src/graphql/mutations/todoMutation.ts
@@ -4,7 +4,9 @@ import TodoModel, { Todo } from '../../models/TodoModel'
 
 export const createTodo = async (root, args) => {
   const { groupName, input } = args
-  if (input.startDate > input.endDate) ErrorMessage('startDate must not be more than endDate')
+  if (input.startDate && input.endDate) {
+    if (input.startDate > input.endDate) ErrorMessage('startDate must not be more than endDate')
+  }
   const todoMaxOrder = await TodoModel.findOne({ groupName }).sort('-order')
   input.order = todoMaxOrder ? todoMaxOrder.order + 1 : 1
   return { status: PAYLOAD_STAATUS.SUCCESS, payload: await TodoModel.create({ ...input, groupName }) }
@@ -12,20 +14,103 @@ export const createTodo = async (root, args) => {
 
 export const updateTodo = async (root, args) => {
   const { todoId, groupName, order, input } = args
-  
-  if (input.startDate > input.endDate) ErrorMessage('startDate must not be more than endDate')
-  const todoList = await TodoModel.find({ groupName, order: { $gte: order } })
+  const oldTodo = await TodoModel.findByIdAndUpdate(todoId)
+  if (!oldTodo) ErrorMessage('Todo not found.')
+  //Validate Time
+  if (input?.startDate && input?.endDate) {
+    if (input?.startDate > input?.endDate) ErrorMessage('startDate must not be more than endDate')
+  }
+  if (input?.startDate && !input?.endDate) {
+    if (input?.startDate > oldTodo.endDate) ErrorMessage('startDate must not be more than endDate')
+  }
+  if (input?.endDate && !input?.startDate) {
+    if (input?.endDate < oldTodo.startDate) ErrorMessage('endDate must not be less than startDate')
+  }
 
-  if (todoList.length > 0) {
+  if (groupName === oldTodo.groupName) {
+    if (order > oldTodo.order) {
+      const todoList = await TodoModel.find({
+        groupName,
+        order: { $gt: oldTodo.order, $lte: order },
+      })
+
+      if (todoList.length > 0) {
+        Promise.all(
+          todoList.map(async (todo: Todo) => {
+            return await TodoModel.findByIdAndUpdate(
+              todo._id,
+              {
+                order: todo.order - 1,
+              },
+              { new: true }
+            )
+          })
+        )
+      }
+    }
+    if (order < oldTodo.order) {
+      const todoList = await TodoModel.find({
+        groupName,
+        order: { $gte: order, $lt: oldTodo.order },
+      })
+      if (todoList.length > 0) {
+        Promise.all(
+          todoList.map(async (todo: Todo) => {
+            return await TodoModel.findByIdAndUpdate(
+              todo._id,
+              {
+                order: todo.order + 1,
+              },
+              { new: true }
+            )
+          })
+        )
+      }
+    }
+  } else {
+    //change new group
+    const todoListNewGroup = await TodoModel.find({
+      groupName,
+      order: { $gte: order },
+    })
+
+    if (todoListNewGroup.length > 0) {
+      Promise.all(
+        todoListNewGroup.map(async (todo: Todo) => {
+          return await TodoModel.findByIdAndUpdate(
+            todo._id,
+            {
+              order: todo.order + 1,
+            },
+            { new: true }
+          )
+        })
+      )
+    }
+
+    //change old group
+    const todoListOldGroup = await TodoModel.find({ groupName: oldTodo.groupName, order: { $gt: oldTodo.order } })
     Promise.all(
-      todoList.map(async (todo: Todo, index) => {
-        await TodoModel.findOneAndUpdate({ _id: todo._id }, { ...todo, order: todo.order + 1 }, { new: true })
+      todoListOldGroup.map(async (todo: Todo) => {
+        return await TodoModel.findByIdAndUpdate(
+          todo._id,
+          {
+            order: todo.order - 1,
+          },
+          { new: true }
+        )
       })
     )
   }
-  const newTodo = await TodoModel.findOneAndUpdate(
-    { _id: todoId },
-    { ...input, groupName, order: todoList.length > 0 ? order : todoList[todoList.length - 1].order + 1 },
+  const newTodoList = await TodoModel.findOne({ groupName }).sort('-order')
+
+  const newTodo = await TodoModel.findByIdAndUpdate(
+    todoId,
+    {
+      ...input,
+      groupName,
+      order: newTodoList && order > newTodoList.order ? newTodoList.order + 1 : order,
+    },
     { new: true }
   )
 
@@ -35,7 +120,7 @@ export const updateTodo = async (root, args) => {
 export const deleteTodo = async (root, { todoId }) => {
   const todo = await TodoModel.findById(todoId)
   if (!todo) ErrorMessage('Todo not found.')
-  const todoList = await TodoModel.find({ groupName: todo.groupName, order: { $gte: todo.order } })
+  const todoList = await TodoModel.find({ groupName: todo.groupName, order: { $gt: todo.order } })
 
   if (todoList.length > 0) {
     Promise.all(
@@ -44,6 +129,6 @@ export const deleteTodo = async (root, { todoId }) => {
       })
     )
   }
-  const afterDeleteTodo = await TodoModel.findByIdAndRemove(todoId)
+  const afterDeleteTodo = await TodoModel.findByIdAndRemove(todoId, { new: true })
   return { status: PAYLOAD_STAATUS.SUCCESS, payload: afterDeleteTodo }
 }
